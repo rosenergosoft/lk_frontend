3eaa16e9a8cf5ec583b67a1f6753b351e1d36cd8
Sviatoslav Zhilichev <tom.d@newizze.com>
2021-07-29 22:17:21 +0300

    vuetify + users page + login improvements

---
+++ pages/users.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="users"
+    :items-per-page="5"
+    class="elevation-1"
+  ></v-data-table>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+  data () {
+    return {
+      headers: [
+        { text: 'Имя', value: 'name' },
+        { text: 'kljad', value: 'asf' },
+        { text: 'asf', value: 'as3f' },
+        { text: 'asf', value: 'fa' }
+      ],
+      users: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
