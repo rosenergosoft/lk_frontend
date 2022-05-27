fd5898f0b79aa91c7d1af05631d54ce0f4425f8a
Works <works@MacBook-Pro-Vladislav.local>
2022-05-27 20:31:12 +0300

    Signature remove

--- components/account/documents/DocumentsItem.vue
+++ components/account/documents/DocumentsItem.vue
@@ -13,16 +13,8 @@
     </div>
     <div v-if="!doc.signature && doc.type !== 'personal_id'">
       <template v-if="doc.user_id === user.id">
-        <a @click.prevent="signDoc"><b-icon-clipboard v-tooltip="'Подписать документ'" class="bootstrap-icon hoverColor mr-0" style="top: 4px;" /></a>
         <a @click.prevent="deleteDocument"><b-icon-trash v-tooltip="'Удалить документ'" class="bootstrap-icon mr-0 red-status" style="top: 4px;" /></a>
       </template>
-      <template v-else>
-        <b-icon-clipboard-x v-tooltip="'Документ не подписан'" class="bootstrap-icon" style="top: 3px; color: red" />
-      </template>
-    </div>
-    <div v-else class="dark-green">
-      <a v-if="!isExecutive" @click.prevent="signDoc"><b-icon-clipboard-check v-tooltip="'Документ подписан'" class="bootstrap-icon" style="top: 3px;" /></a>
-      <div v-else><b-icon-clipboard-check v-tooltip="'Документ подписан'" class="bootstrap-icon" style="top: 3px;" /></div>
     </div>
     <Signing
       :doc="doc"
